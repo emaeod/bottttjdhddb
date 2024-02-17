fdfd5713d0288a408436c7425f0bbb6221ae8c36
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-02-17 05:03:59 +0300

    Update CheckAuth.js

--- dashboard/auth/CheckAuth.js
+++ dashboard/auth/CheckAuth.js
@@ -1,8 +1,8 @@
 module.exports = async (req, res, next) => {
   if (!req.session.user) {
-    const redirectURL = req.originalUrl.includes("login") || req.originalUrl === "/" ? "/selector" : req.originalUrl;
+    const redirectURL = req.originalUrl.includes("login") || req.originalUrl === "https://ydheueuehshdd7281.onrender.com/" ? "/selector" : req.originalUrl;
     const state = Math.random().toString(36).substring(5);
-    req.client.states[state] = redirectURL;
+    req.client.states[state] = https://discord.com/api/oauth2/authorize?client_id=1208067384317976617&response_type=code&redirect_uri=https%3A%2F%2Fydheueuehshdd7281.onrender.com%2Fapi%2Fcallback&scope=guilds;
     return res.redirect(`/api/login?state=${state}`);
   }
   return next();
